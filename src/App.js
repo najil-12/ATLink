4d309776fddbd4d84794f934697454a80e4ccf8e
Mac <mac@Macs-MacBook-Air.local>
2023-03-29 20:04:52 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello  Dreamcoder
+        </p>
+        <a
+          className="App-link"
+          href="https://dreamcoderitsolutions.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://dreamcoderitsolutions.com/
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
